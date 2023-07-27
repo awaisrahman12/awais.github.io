2981a89a27b6d532c713602cfab553b5661f027f
awaisrahman12 <awaisrahman011@gmail.com>
2023-07-27 14:54:54 +0500

    My Personal Portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Awais ul Rahman</span> With my strong passion for UI design, attention to detail, and ability to create visually stunning and user-friendly interfaces, I am confident in my ability to contribute to the success of your organization, also as a highly qualified front-end developer, I have a strong enthusiasm for building beautiful, intuitive websites. I have the skills to turn designs and thoughts into interesting digital experiences since I have a solid foundation in HTML, CSS, and JavaScript. "
+
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
